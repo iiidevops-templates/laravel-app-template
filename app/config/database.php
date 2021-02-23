10394143f3a04dc716ec1812831ad3c02aad0ccd
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-02-23 06:37:41 +0000

    fix

--- app/config/database.php
+++ app/config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('db_host', '127.0.0.1'),,
+            'host' => env('db_host', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('db_name', 'forge'),
             'username' => env('db_username', 'forge'),
