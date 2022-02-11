a4c9cb90c297019ace3547bd4d4330c9f0f6edee
jonathan <tryweb@ichiayi.com>
2022-02-11 18:23:20 +0800

    改回官網 github 版本 - https://github.com/laravel/laravel Commits on Feb 10, 2022

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('home/', HomeController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
